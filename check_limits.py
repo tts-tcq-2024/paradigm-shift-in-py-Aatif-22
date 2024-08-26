0f0630f7ba1cba440089a486da2294c39ac6f43e
Aatif-22 <mdaatifshafique54@gmail.com>
2024-08-26 15:52:37 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,7 +3,7 @@ def battery_is_ok(temperature, soc, charge_rate):
   if temperature < 0 or temperature > 45:
     print('Temperature is out of range!')
     return False
-  elif soc < 20 or soc > 80:
+  elif soc < 20 or soc > 80: 
     print('State of Charge is out of range!')
     return False
   elif charge_rate > 0.8:
